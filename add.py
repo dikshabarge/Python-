5a6e979c8f068dbb1b8625e6507b97cd51cf8779
dikshabarge <157716437+dikshabarge@users.noreply.github.com>
2024-02-08 17:31:27 +0530

    add.py
    Python Program To Add Two Numbers

---
+++ add.py
@@ -0,0 +1,13 @@
+# Python Program To Add Two Numbers
+
+num1 = 14
+num2 = 23
+add = num1 + num2
+print("The addition of two number is:", add)
+
+
+#Add Two Numbers With User Input
+num1 = input("Enter First Number: ")
+num2 = input("Enter Second Number: ")
+add = int(num1) + int(num2)
+print("The addition of Two number is: ", add)
